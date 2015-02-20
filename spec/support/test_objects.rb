0f0bd6ab222d15ab5f70cc02e7b2471fd17311f4
pocohideout <pocohideout@users.noreply.github.com>
2015-02-20 01:58:13 -0800

    Refactor TestObjects module

--- spec/support/test_objects.rb
+++ spec/support/test_objects.rb
@@ -1,28 +1,30 @@
 module TestObjects
-  # Returns a valid Stat
+  # Return a valid Stat
   def self.stat
-    Stat.new(
+    Stat.new(attributes)
+  end
+
+  # Create and return a list of Stats
+  def self.stats!(count)
+    list = []
+    count.times do |i|
+      attr = attributes
+      attr[:question] += " #{i}"
+      list << Stat.create!(attr)
+    end
+    list
+  end
+  
+  private
+  
+  def self.attributes
+    {
       category: :science,
       question: 'Some question over ___ characters long',
       answer: '20.0',
       source: 'UC Berkeley',
       year: 2015,
       link: 'http://www.somelink.com'
-    )
-  end
-
-  def self.stats!(count)
-    list = []
-    count.times do |i|
-      list << Stat.create!(
-        category: :science,
-        question: "Some question over ___ characters long #{i}",
-        answer: '20.0',
-        source: 'UC Berkeley',
-        year: 2015,
-        link: 'http://www.somelink.com'
-      )
-    end
-    list
+    }
   end
 end
